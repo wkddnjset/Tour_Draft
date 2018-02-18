2a1388f4bb4832ffa6dbbf5ffd8f371c44d67964
Jangwon <wkddnjset@gmail.com>
2018-02-18 15:07:55 +0900

    update

--- django/django-src/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BlogPost, image
-# Register your models here.
-class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title')
-
-admin.site.register(BlogPost, BlogPostAdmin)
-admin.site.register(image)
