053b71570699061acca30aa68afe494743ace16f
Jangwon <wkddnjset@gmail.com>
2018-03-04 14:17:22 +0900

    Item Detail API 추가

--- django/src/_Item/api/urls.py
+++ django/src/_Item/api/urls.py
@@ -1,6 +1,7 @@
-from .views import ItemListView
+from .views import *
 from django.conf.urls import url
 
 urlpatterns = [
     url(r'^items/$', ItemListView.as_view(), name='items'),
+    url(r'^items/(?P<id>\d+)/$', ItemDetailView.as_view(), name='items-detail'),
 ]
--- django/src/_Item/api/views.py
+++ django/src/_Item/api/views.py
@@ -1,11 +1,17 @@
 from rest_framework import generics
-from _Item.models import Item
+from ..models import Item
 from .serializers import ItemListSerializers
 
 class ItemListView(generics.ListAPIView):
     lookup_field = 'id'
     serializer_class = ItemListSerializers
-    # queryset = BlogPost.objects.all()
+
+    def get_queryset(self):
+        return Item.objects.all()
+
+class ItemDetailView(generics.RetrieveAPIView):
+    lookup_field = 'id'
+    serializer_class = ItemListSerializers
 
     def get_queryset(self):
         return Item.objects.all()
